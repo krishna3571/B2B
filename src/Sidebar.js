f4e2985a79719206af3d5a0abaa0e21e6a764058
HemangKatariya <Hemangk50@gmail.com>
2024-01-16 18:59:31 +0530

    sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,9 @@
-
-
 import React, { useState } from 'react';
 import '../src/css/Sidebar.css'; // Import your CSS file
+import { FaBars, FaUser } from 'react-icons/fa';
+
+import { Link, useNavigate } from "react-router-dom";
+
 
 const YourComponent = () => {
     const [isNavbarOpen, setNavbarOpen] = useState(false);
@@ -18,25 +20,44 @@ const YourComponent = () => {
         <>
             <nav className={isNavbarOpen ? 'open' : ''}>
                 <div className="logo" onClick={toggleNavbar}>
-                    <i className={`bx ${isNavbarOpen ? 'bx-x' : 'bx-menu'} menu-icon`}>H</i>
+                    <i className={`bx ${isNavbarOpen ? 'bx-x' : 'bx-menu'} menu-icon`}>
+                        <FaBars />
+                    </i>
                     <span className="logo-name">CodingLab</span>
                 </div>
                 <div className="sidebar">
                     {/* Sidebar content goes here */}
                     <div className="logo" onClick={closeNavbar}>
                         <i className="bx bx-menu menu-icon"></i>
+                        <FaBars />
                         <span className="logo-name">CodingLab</span>
                     </div>
 
                     <div className="sidebar-content">
                         <ul className="lists">
-                            {/* Sidebar list items go here */}
+                            <a href="#" className="nav-link">
+                                <i className="bx bx-cog icon">
+                                    {isNavbarOpen ? <FaUser /> : <FaUser />}
+                                </i>
+                                <span className="link">EADD USER</span>
+                            </a>
+
+                            <a href="#" className="nav-link">
+                                <i className="bx bx-cog icon">
+                                    <Link  to="/profile">
+                                        {isNavbarOpen ? <FaUser /> : <FaUser />}
+                                    </Link>
+                                </i>
+                                <span className="link">Settings</span>
+                            </a>
                         </ul>
 
                         <div className="bottom-content">
                             <li className="list">
                                 <a href="#" className="nav-link">
-                                    <i className="bx bx-cog icon"></i>
+                                    <i className="bx bx-cog icon">
+
+                                    </i>
                                     <span className="link">Settings</span>
                                 </a>
                             </li>
